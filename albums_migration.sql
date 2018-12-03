1830766e7cc5d2b107483b9bfc8ae4fc2f2bd2dd
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-12-03 13:48:32 -0600

    created where exercises file and completed the first round of exercises. Also updated changed the data type for the date column in albums migration file.

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   artist VARCHAR(100) NOT NULL,
   name VARCHAR(50) NOT NULL,
-  release_date DATE,
+  release_date INT,
   sales DOUBLE,
   genre VARCHAR(50) NOT NULL,
   PRIMARY KEY(id)

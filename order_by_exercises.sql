86f866821af5f0a8879c2e8901d45f1d83988022
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-12-03 14:47:37 -0600

    completed order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,9 +4,12 @@ SELECT * FROM employees
 WHERE (first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya')
-AND gender = 'M';
+AND gender = 'M'
+ORDER BY last_name DESC, first_name DESC;
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
 
 SELECT * FROM employees WHERE hire_date LIKE '199%';
 
@@ -24,7 +27,8 @@ AND last_name LIKE '%e';
 
 SELECT * FROM employees WHERE
 hire_date LIKE "199%"
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY hire_date DESC, birth_date DESC;
 
 SELECT * FROM employees WHERE
 last_name LIKE '%q%'

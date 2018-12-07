72385bd106fa936eddbe7af9208201e2be4dc63d
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-12-07 00:21:05 -0600

    updated join exercise.

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,3 +1,13 @@
 USE employees;
 
-SELECT * FROM users;
+# write a query that shows each department along with
+# the name of the current manager for that department.
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM departments AS d
+JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no
+JOIN employees AS e
+ON dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY d.dept_name;
